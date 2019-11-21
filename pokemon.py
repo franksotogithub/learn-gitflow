df8a93bba107ddb7c9c06312954c50acc7e0d8f7
Jose Ramirez <jcramireztello@gmail.com>
2019-11-21 18:55:17 -0500

    feat(pokemon): Agregando clase
    Se agrego la clase principal pokemon con
    atributos relevantes
    
    Task f001

---
+++ pokemon.py
@@ -0,0 +1,11 @@
+class Pokemon:
+    def __init__(nombre, fuerza=0):
+        self.nombre = nombre
+        self.fuerza = fuerza
+
+    def get_nombre():
+        return self.nombre
+
+    def get_fuerza():
+        return self.fuerza
+
